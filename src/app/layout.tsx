94c0935a4d10f3850ba4b4cd32f7390a89e17880
Chia <lovemaple0913@gmail.com>
2025-03-10 23:35:22 +0800

    chore: remove useless pkgs/files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
 
-import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from "@mantine/core";
 import type { Metadata } from "next";
-
-import theme from "@/libs/theme";
 
 export async function generateMetadata(): Promise<Metadata> {
   const faviconPath = `/favicon.ico`;
@@ -24,13 +21,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body className="antialiased">
-        <MantineProvider theme={theme}>{children}</MantineProvider>
-      </body>
+    <html lang="en">
+      <head></head>
+      <body>{children}</body>
     </html>
   );
 }
